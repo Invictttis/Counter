470ebc3d62d442564cddc9797ff4fd678f666b63
Ivan Kornev <invictis2123@gmail.com>
2023-10-24 18:11:44 +0400

    Добавил кнопку при нажатии на которую в Label появляется значение, равное тому, сколько раз была нажата кнопка

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var x: Int = 0
 
+    @IBOutlet weak var labelForCounting: UILabel!
+    
+    
+    @IBOutlet weak var countButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        labelForCounting.text = "Значение счетчика: 0"
+        
     }
 
-
+    @IBAction func pressButton(_ sender: Any) {
+        x = x + 1
+        print(x)
+        labelForCounting.text = "Значение счетчика: \(x)"
+    }
+    
 }
 
